dd0206a337e2f4188ecfba3a83fb559000960aaa
Jollu Ashok Kumar Yadav <114097476+Ashok0757@users.noreply.github.com>
2023-08-18 10:27:51 +0530

    Java Practice Programs

---
+++ LabledLoops/src/LabledForLoop.java
@@ -0,0 +1,16 @@
+
+public class LabledForLoop {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Ashok:for(int i=1;i<=5;i++) {
+			Yadav:for(int j=1;j<=5;j++) {
+				System.out.print("kumar");
+				j++;
+				break Ashok;
+			}
+			System.out.println();
+			i++;
+			}
+		}
+	}
